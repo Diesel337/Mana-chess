e3d6578f0a2665605908a60f38c7aa38416e76a3
David <david@example.local>
2026-07-16 10:47:07 -0600

    Allow Railway health checks before SSL redirect

--- mana_chess_online/config/prod.exs
+++ mana_chess_online/config/prod.exs
@@ -17,7 +17,7 @@ config :mana_chess_online, ManaChessOnlineWeb.Endpoint,
   force_ssl: [
     rewrite_on: [:x_forwarded_proto],
     exclude: [
-      # paths: ["/health"],
+      paths: ["/health"],
       hosts: ["localhost", "127.0.0.1"]
     ]
   ]
